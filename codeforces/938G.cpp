b6835f4dff8ff907948f92e36a37fc71fc471024
cjdjr <2609943977@qq.com>
2018-03-11 15:30:18 +0800

    Update 938G.cpp

--- codeforces/938G.cpp
+++ codeforces/938G.cpp
@@ -9,22 +9,31 @@ struct wjmzbmr
     {
         scanf("%d%d%d",&u,&v,&w);
     }
-}e[maxn+5];
+}e[maxn*2+5];
 map<pair<int,int> , int> M;
-int n,m,Q,len,tot=1;
-vector<pair<int,int> > q[maxn+5];
-vector<int>g[maxn*4+5];
-int f[maxn+5],sz[maxn+5];
+int n,m,Q,len,tot,top;
+pair<int,int> q[maxn+5];
+vector<int> g[maxn*4+5];
+int f[maxn+5],sz[maxn+5],val[maxn+5];
+int Stack[maxn*2+5];
+int base[35][35];
 int find(int x)
 {
     if(f[x]==x) return x;
     int father=find(f[x]);
-    val[x]=val[x]^val[f[x]];
-    return f[x]=father;
+    return father;
 }
+int get_val(int x)
+{
+    int w=0;
+    while (f[x]!=x) w^=val[x],x=f[x];
+    return w;
+}
+
 void change(int k,int l,int r,int x,int y,int id)
 {
-    if(l>r) return;
+    if(x>y) return;
+    if(l>r||l>y||r<x) return;
     if(x<=l&&r<=y)
     {
         g[k].push_back(id);
@@ -46,11 +55,13 @@ void ins(int k,int dep)
             if(sz[u]>sz[v]) swap(u,v),swap(x,y);
             f[u]=v;
             sz[v]+=sz[u];
-            val[u]=val[x]^val[y]^w;
+           val[u]=0;
+           val[u]=get_val(x)^get_val(y)^w;
+            Stack[++top]=u;
         }
         else
         {
-            w^=val[x]^val[y];
+            w=get_val(x)^get_val(y)^w;
             for(int i=30;i>=0;--i)
                 if(w&(1<<i))
                     if(base[dep][i]==0)
@@ -64,14 +75,41 @@ void ins(int k,int dep)
         }
     }
 }
+void del(int t)
+{
+    while(top>t)
+    {
+        int u=Stack[top--];
+        sz[f[u]]-=sz[u];
+        val[u]=0;
+        f[u]=u;
+    }
+}
 void solve(int k,int l,int r,int dep)
 {
     if(l>r) return;
-    ins(k);
+    int tmp=top;
+    ins(k,dep);
+    if(l==r)
+    {
+        int x=q[l].first,y=q[l].second;
+        int ans=get_val(x)^get_val(y);
+        for(int i=30;i>=0;--i)
+                if(base[dep][i])
+                    if(ans&(1<<i)) ans^=base[dep][i];
+        printf("%d\n",ans);
+        del(tmp);
+        return;
+    }
+    int mid=(l+r)>>1;
+    for(int i=0;i<=30;++i) base[dep+1][i]=base[dep][i];
+    solve(k*2,l,mid,dep+1);
+    for(int i=0;i<=30;++i) base[dep+1][i]=base[dep][i];
+    solve(k*2+1,mid+1,r,dep+1);
+    del(tmp);
 }
 int main()
 {
-    freopen("ce.in","r",stdin);
     scanf("%d%d",&n,&m);
     for(int i=1;i<=m;++i)
     {
@@ -81,6 +119,7 @@ int main()
         M[mp(e[len].u,e[len].v)]=len;
     }
     scanf("%d",&Q);
+    tot=0;
     for(int i=1;i<=Q;++i)
     {
         int op,x,y,d;
@@ -88,7 +127,7 @@ int main()
         if(op==1)
         {
             scanf("%d%d%d",&x,&y,&d);
-            e[++len]={x,y,d,tot,-1};
+            e[++len]={x,y,d,tot+1,-1};
             M[mp(x,y)]=len;
         }
         else
@@ -96,16 +135,17 @@ int main()
             {
                 scanf("%d%d",&x,&y);
                 e[M[mp(x,y)]].r=tot;
-                ++tot;
+                M[mp(x,y)]=0;
             }
             else
             {
                 scanf("%d%d",&x,&y);
-                q[tot].push_back(mp(x,y));
+                q[++tot]=mp(x,y);
             }
     }
+    for(int i=1;i<=len;++i) if(e[i].r==-1) e[i].r=tot;
     for(int i=1;i<=len;++i) change(1,1,tot,e[i].l,e[i].r,i);
     for(int i=1;i<=n;++i) f[i]=i,sz[i]=1;
-    solve(1,1,tot,0);
+    if(tot) solve(1,1,tot,0);
     return 0;
 }
